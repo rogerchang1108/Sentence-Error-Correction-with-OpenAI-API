7d8465b00aa28f02ea064d513eb23f20e1cc3315
rogerchang1108 <rogerchang1108@gmail.com>
2024-03-10 14:51:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Sentence Error Correction
+
+In this project, utilizing Python and OpenAI API to develop a Streamlit web focused on correcting sentence error.
+
+## Features
+
+### Usage
+
+1. The website's session state is **locked** without an **OpenAI API Key**. <br> 
+You can apply an OpenAI API Key on [OpenAI](https://openai.com/blog/openai-api).
+
+   ![component](img/locked.png)
+
+2. After inputing a key, the session will be **unlock**.<br>
+You can input **sentence that you want to correct** now. <br>
+(Hint: you can adjust the **prompt** by yourself if you would like to. Try it now!)
+
+   ![component](img/unlock.png)
+
+3. You may also change the OpenAI model. The default model is **gpt-3.5-turbo-0125**
+    
+    ![component](img/choose_models.png)
+
+4. Click the **"Submit"** button, you will get the corrected sentence in couple seconds.
+
+   ![component](img/result.png)
+
+
+### Python Module Required & Version
+
+- openai: 0.28.0 
+- streamlit: 1.28.2
+- streamlit-lottie: 0.0.5
+
+### How I Design the Prompt
+
+- Assuming Roles
+- Assign Tasks and Details
+- Chain of Thought Prompts
+- Specify Output Format
+- Provide Few Shots Examples
+
+## Resources
+
+- [List](https://api.flutter.dev/flutter/dart-core/List-class.html)
+- [TextStyle](https://api.flutter.dev/flutter/painting/TextStyle-class.html) and [GoogleFonts](https://pub.dev/packages/google_fonts)
+- [BoxDecoration](https://api.flutter.dev/flutter/painting/BoxDecoration-class.html)
---
+++ main.py
@@ -0,0 +1,132 @@
+import json
+import openai
+import requests 
+
+import streamlit as st
+from streamlit_lottie import st_lottie 
+
+# openai.api_key = ''
+
+def correct_error(prompt, input_content, model_selected):
+
+    content_statement = prompt
+    
+    content_input = f"""{input_content}"""
+	
+    response = openai.ChatCompletion.create(
+        model = model_selected,
+        messages = [
+            {'role': 'user', 'content': content_statement},
+            {'role': 'user', 'content': content_input}
+        ],
+        temperature = 0.7
+    )
+
+    response_content = response['choices'][0]['message']['content']
+    
+    st.write(f"""{model_selected}'s response:""")
+    st.write(response_content)
+
+tab1, tab2 = st.tabs(["Sentence Error Correction", "Surprise!!!"])
+
+with tab1:
+    st.title("Sentence Error Correction")
+    
+    if 'disabled' not in st.session_state:
+        st.session_state.disabled = True
+    
+    input_api_key = st.text_input('OpenAI API Key:')
+    openai.api_key = input_api_key
+    
+    if input_api_key:
+        st.success('Unlocked!', icon = '🔓')
+        st.session_state.disabled = False
+    else:
+        st.info('Locked: Please Input Your OpenAI API Key First.', icon = '🔐')
+        st.session_state.disabled = True
+      
+    with st.form(key='my_form'):
+        prompt = st.text_area(
+            'Prompt for ChatGPT (You Can Adjust it if you want)',
+            f"""Act as a English teacher.
+            
+Correct the input sentences. There may be article, vocabulary choosing, spelling or phase choosing error, which make the meaning of sentence incorrect and ambiguous. 
+
+Correct the sentence step by step.
+Output only the corrected sentence.
+
+For examples: 
+Input: I hope you all have a enjoyable stay.
+Output: I hope you all have an enjoyable stay.
+
+Input: One of the girls I share with is a British.
+Output: One of the girls I share with is British.
+    
+Input:  It is difficult to reach abandoned places such as small country villages.
+Output: It is difficult to reach remote places such as small country villages.
+
+Input:  I want to improve my ability of English.
+Output: I want to improve my ability in English.
+
+Input:  We weren't able to stop laughing.
+Output: We couldn't stop laughing.
+    
+Input:  He likes reading, above all novels.
+Output: He likes reading, especially novels.
+    
+Input:  It's a pity that you were absent from the training session.
+Output: It's a pity that you weren't at the training session.
+    
+Here is the input:""",
+            height = 190,
+            disabled = st.session_state.disabled
+        )   
+    
+        input = st.text_input(
+            'Input Incorrect Sentence and Press "Submit" to Get Result',
+            'I hope you all have a enjoyable stay.',
+            disabled = st.session_state.disabled
+        )
+    
+        model_selected = st.selectbox(
+            'Model',
+            ['gpt-3.5-turbo-0125', 'gpt-3.5-turbo-1106', 'gpt-4-0125-preview', 'gpt-4-1106-preview'],
+            disabled = st.session_state.disabled
+        )
+    
+        submit_button = st.form_submit_button(
+            label='Submit', 
+            disabled=st.session_state.disabled
+        )
+
+    if submit_button:
+        correct_error(prompt, input, model_selected)
+
+with tab2:
+    url = requests.get( 
+    "https://lottie.host/4ce71fb9-e943-45ae-a81d-04435c1e4323/wMTe8jOfPu.json") 
+
+    url_json = dict() 
+  
+    if url.status_code == 200: 
+        url_json = url.json() 
+    else: 
+        print("Error in the URL") 
+  
+    st.title("Please Give Me a Star If You Like It")
+    
+    st_lottie(url_json, 
+        # change the direction of our animation 
+        reverse = True, 
+        # height and width of animation 
+        height = 400,   
+        width = 400, 
+        # speed of animation 
+        speed = 0.8,   
+        # means the animation will run forever like a gif, and not as a still image 
+        loop = True,   
+        # quality of elements used in the animation, other values are "low" and "medium" 
+        quality = 'high', 
+        # This is just to uniquely identify the animation 
+        key = 'Cat' 
+    ) 
